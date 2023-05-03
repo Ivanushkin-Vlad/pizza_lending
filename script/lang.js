6e294bde92605479829e8de4be1d4c0fe52016d0
Ivanushkin-Vlad <126400653+Ivanushkin-Vlad@users.noreply.github.com>
2023-05-03 16:00:08 +0300

    init animation

--- script/lang.js
+++ script/lang.js
@@ -54,5 +54,53 @@ const langArr = {
     "about-1": {
         "en": "About us",
         "ua": "Про нас",
-    }
+    },
+    "treker" :  {
+        "en": "What is the Pizza Tracker service:",
+        "ua": "Що таке сервіс Pizza Tracker:",
+    }, 
+    "p1" :  {
+        "en": "1. In the process:",
+        "ua": "1. В процесі:",
+    }, 
+    "pO" :  {
+        "en": "the manager confirmed the order.",
+        "ua": "менеджер підтвердив замовлення.",
+    }, 
+    "p2": {
+        "en": "2. Makeline",
+        "ua": "2. Makeline",
+    }, 
+    "pD": {
+        "en": "the necessary ingredients are already laid out on the dough.",
+        "ua": "на тісто вже викладають потрібні інгредієнти.",
+    }, 
+    "p3": {
+        "en": "3. In the box:",
+        "ua": "3. У коробці:",
+    },
+    "pDisches": {
+        "en": "pizzaiolo finally checks whether the dish meets the requirements and cuts it into pieces",
+        "ua": "піцейоло остаточно перевіряють, чи відповідає страва вимогам, та нарізають її на шматки.",
+    },
+    "p4": {
+        "en": "4. On the road:",
+        "ua": "4. У дорозі:",
+    },
+    "pDeliv": {
+        "en": "the courier has already left with your order and will be with you soon.",
+        "ua": "кур’єр вже вирушив із вашим замовленням і незабаром буде у вас.",
+    },
+    "mainDesc": {
+        "en": "In order not to wait for the courier for an extra time, it is better to order in advance. You can timely choose everything necessary for the order and determine the time when the courier is available drive up",
+        "ua": "Щоби не очікувати кур’єра зайвий час, краще замовляти заздалегідь. Ви можете своєчасно обрати все необхідне до замовлення й визначити час, коли кур’єр має під’їхати.",
+    },
+    "form": {
+        "en": "Client form",
+        "ua": "Форма клієнта",
+    },
+    "sub": {
+        "en": "Send",
+        "ua": "Відправити",
+    },
 }
--- script/main.js
+++ script/main.js
@@ -1,18 +1,61 @@
+// let pizzaSale = [
+//     {
+//         "id": "001",
+//         "name": "Челінтано (570гр.)",
+//         "price": "139 грн.",
+//         "imgSrc": "./image/Chelentano_pizza.png"
+//     },
+//     {
+//         "id": "002",
+//         "name": "Маргарита (520гр.)",
+//         "price": "179 грн.",
+//         "imgSrc": "./image/margarita-pizza.png"
+//     },
+//     {
+//         "id": "003",
+//         "name": "Обалдіні (550гр.)",
+//         "price": "219 грн.",
+//         "imgSrc": "./image/obaldini-pizza.png"
+//     }
+// ];
+
+
 let btn = document.querySelector('.logo_language_menu');
 let popupBg = document.querySelector('.popup_lang');
 
-btn.addEventListener('click',()=>{
-    popupBg.classList.toggle('active');    
+let leftBtn = document.querySelector(".buttoms_left");
+let rightBtn = document.querySelector(".buttoms_right");
+
+let pizzaName = document.querySelector('.pizza_name');
+let pizzaPrice = document.querySelector('.pizza_price');
+
+let pizzaPhoto = document.querySelector('.pizza_logo');
+
+// rightBtn.addEventListener('click', () => {
+//        handlerPizza()
+// })
+
+// function handlerPizza(){ 
+//     for(i = 0; i < pizzaSale.length; i++){
+//         pizzaPhoto.src = pizzaSale[i].imgSrc;
+//         pizzaName.innerHTML = pizzaSale[i].name;
+//         break;
+//     }
+
+// }
+
+btn.addEventListener('click', () => {
+    popupBg.classList.toggle('active');
 })
 
 
-popupBg.addEventListener('click', (e)=>{
-    if(e.target === popupBg){
-    popupBg.classList.remove('active');
+popupBg.addEventListener('click', (e) => {
+    if (e.target === popupBg) {
+        popupBg.classList.remove('active');
     }
 })
 
-popupBg.addEventListener('click',(e)=>{
+popupBg.addEventListener('click', (e) => {
     let option = e.target.innerHTML;
     changeURLLanguage(option);
 
@@ -42,3 +85,4 @@ function changeLanguage() {
 }
 
 changeLanguage();
+AOS.init();
